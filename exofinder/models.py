7900e6cade529769e44221b220112f90ed87dba5
Pancor <pancordev@gmail.com>
2022-08-05 23:08:46 +0200

    train: configured multiple input model.

--- exofinder/models.py
+++ exofinder/models.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 def get_local_view_model(show_summary=False):
     model = tf.keras.Sequential()
 
-    model.add(tf.keras.layers.Reshape(target_shape=(201, 1), input_shape=(201,)))
+    model.add(tf.keras.layers.Reshape(target_shape=(201, 1), input_shape=(201,), name="local_flux"))
     model.add(tf.keras.layers.Conv1D(8, 3, activation=tf.nn.relu))
     model.add(tf.keras.layers.MaxPool1D(pool_size=2))
     model.add(tf.keras.layers.Flatten())
@@ -20,7 +20,7 @@ def get_local_view_model(show_summary=False):
 def get_global_view_model(show_summary=False):
     model = tf.keras.Sequential()
 
-    model.add(tf.keras.layers.Reshape(target_shape=(2001, 1), input_shape=(2001,)))
+    model.add(tf.keras.layers.Reshape(target_shape=(2001, 1), input_shape=(2001,), name="global_flux"))
     model.add(tf.keras.layers.Conv1D(8, 3, activation=tf.nn.relu))
     model.add(tf.keras.layers.MaxPool1D(pool_size=2))
     model.add(tf.keras.layers.Flatten())
--- train.py
+++ train.py
@@ -12,28 +12,39 @@ def set_shapes(x, y, sample_weights):
     return x, y, tf.reshape(sample_weights, (len(sample_weights),))
 
 
-local_train_dataset = exofinder.datasets.get_flux_series(path_to_files="./tfrecord/train-*")
-local_valid_dataset = exofinder.datasets.get_flux_series(path_to_files="./tfrecord/val-*")
+def test(x, y):
+    return {
+        "local_flux_input": x[0],
+        "global_flux_input": y
+    }, x[1], x[2]
 
+
+def test_valid(x, y):
+    return {
+               "local_flux_input": x[0],
+               "global_flux_input": y
+           }, x[1]
+
+local_train_dataset = exofinder.datasets.get_flux_series(path_to_files="./tfrecord/train-*")
 global_train_dataset = exofinder.datasets.get_flux_series(path_to_files="./tfrecord/train-*",
                                                           flux_type="global")
+local_valid_dataset = exofinder.datasets.get_flux_series(path_to_files="./tfrecord/val-*")
 global_valid_dataset = exofinder.datasets.get_flux_series(path_to_files="./tfrecord/val-*",
                                                           flux_type="global")
 
-weights = exofinder.utils.weights.calculate_weights(dataset=local_train_dataset)
+#weights = exofinder.utils.weights.calculate_weights(dataset=local_train_dataset)
+weights = np.array([0.64932, 2.17427])
 local_train_dataset = exofinder.utils.weights.add_weights(dataset=local_train_dataset, weights=weights)
-local_valid_dataset = exofinder.utils.weights.add_weights(dataset=local_valid_dataset, weights=weights)
 
 local_view_model = exofinder.models.get_local_view_model()
 global_view_model = exofinder.models.get_global_view_model()
-
-combined_input = np.concatenate([local_view_model.output, global_view_model.output])
+combined_input = tf.keras.layers.concatenate([local_view_model.output, global_view_model.output])
 
 common_layers = tf.keras.layers.Dense(4, activation=tf.nn.relu)(combined_input)
 common_layers = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)(common_layers)
-final_model = tf.keras.Model(inputs=[local_view_model.input, global_view_model.input], output=common_layers)
+final_model = tf.keras.Model(inputs=[local_view_model.input, global_view_model.input], outputs=common_layers)
 
-local_view_model.compile(
+final_model.compile(
     optimizer=tf.keras.optimizers.Adam(),
     loss=tf.keras.losses.BinaryCrossentropy(),
     metrics=[
@@ -45,18 +56,37 @@ log_dir = "./logs/fit/" + datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 file_writer_cm = tf.summary.create_file_writer(log_dir + "/cm")
 
-confusion_matrix_callback = ConfusionMatrixCallback(log_dir, local_valid_dataset)
+#local_train_dataset = local_train_dataset.batch(32)
+#local_train_dataset = local_train_dataset.map(lambda x, y, weight: (x, y, tf.reshape(weight, (len(weight),))))
+#
+# global_train_dataset = global_train_dataset.batch(32)
+
+final_dataset = tf.data.Dataset.zip((local_train_dataset, global_train_dataset))
+final_dataset = final_dataset.map(test)
+
+# for element in final_dataset.as_numpy_iterator():
+#     print(element)
+#     break
+
+# final_dataset = final_dataset.map(map_func=lambda x, y: new_py_function(
+#     func=test, inp=[x, y], Tout=({"local_flux_input": tf.float32, "global_flux_input": tf.float32}, tf.int8, tf.float32)
+# ))
+final_dataset = final_dataset.batch(32)
+final_dataset = final_dataset.map(lambda x, y, weight: (x, y, tf.reshape(weight, (len(weight),))))
+
+valid_dataset = tf.data.Dataset.zip((local_valid_dataset, global_valid_dataset))
+valid_dataset = valid_dataset.map(test_valid)
+valid_dataset = valid_dataset.batch(32)
+
+# final_model.summary()
+
+confusion_matrix_callback = ConfusionMatrixCallback(log_dir, valid_dataset)
 # confusion_matrix_callback.set_showing_confusion_matrix(True)
 
-local_train_dataset = local_train_dataset.batch(32)
-local_valid_dataset = local_valid_dataset.batch(32)
-local_train_dataset = local_train_dataset.map(lambda x, y, weight: (x, y, tf.reshape(weight, (len(weight),))))
-local_valid_dataset = local_valid_dataset.map(set_shapes)
-
-local_view_model.fit(
-    x=local_train_dataset,
+final_model.fit(
+    x=final_dataset,
     epochs=50,
-    validation_data=local_valid_dataset,
+    validation_data=valid_dataset,
     callbacks=[
         tensorboard_callback,
         confusion_matrix_callback
